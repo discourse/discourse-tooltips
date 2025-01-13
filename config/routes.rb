2396399542305405fdcad5b6b59b97ad5943ebaf
David Taylor <david@taylorhq.com>
2025-01-13 20:13:46 +0000

    DEV: Simplify plugin implementation (#81)
    When this plugin was originally created, we were apprehensive about shipping excerpts for every topic in the main topic-list payload. Therefore, this plugin fetched the excerpts asynchronously, with all the associated batching, caching and rate-limiting logic.
    
    Nowadays, it is very common for Discourse themes and plugins to enable excerpt-shipping in the topic-list serializer. Using this technique makes things much simpler, and much simpler for users.
    
    Note: this plugin still uses JQuery, and is incompatible with the new topic-list implementation. Those things will be improved in follow-up commits.

--- config/routes.rb
+++
@@ -1,3 +0,0 @@
-# frozen_string_literal: true
-
-DiscourseTooltips::Engine.routes.draw { get "/" => "tooltip_previews#index" }
---
+++ spec/system/tooltip_spec.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+RSpec.describe "Discourse Tooltips", type: :system do
+  before { SiteSetting.tooltips_enabled = true }
+
+  fab!(:topic1) do
+    Fabricate(:topic).tap do |t|
+      PostCreator.new(t.user, topic_id: t.id, raw: "post for topic one").create.rebake!
+    end
+  end
+
+  fab!(:topic2) do
+    Fabricate(:topic).tap do |t|
+      PostCreator.new(t.user, topic_id: t.id, raw: "post for topic two").create.rebake!
+    end
+  end
+
+  it "renders tooltips correctly" do
+    visit "/latest"
+
+    expect(page).to have_css(".topic-list-item", count: 2)
+    expect(page).not_to have_css(".d-tooltip")
+
+    find("tr[data-topic-id='#{topic1.id}'] .raw-topic-link").hover
+    expect(page).to have_css(".d-tooltip", text: "post for topic one")
+
+    find("tr[data-topic-id='#{topic2.id}'] .raw-topic-link").hover
+    expect(page).to have_css(".d-tooltip", text: "post for topic two")
+  end
+end
